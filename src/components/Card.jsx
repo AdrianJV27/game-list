b3b5e46fae894096f9a6ad56cb4ba86d70e532a3
Adrián Jurado <adrianjjvv@gmail.com>
2025-06-21 10:48:52 +0200

    image position

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,14 +10,16 @@ export function Card({
       <div className="card-image">
         <img src={game.image} alt="" />
       </div>
-      <h2>{game.name}</h2>
-      <div className="card-action">
-        <button onClick={() => toggleFavorites(game)}>
-          {isInFavorites(game) ? '🩷' : '💔'}
-        </button>
-        <button onClick={() => togglePlayed(game)}>
-          {isInPlayed(game) ? '✅' : '❎'}
-        </button>
+      <div style={{ marginTop: 'auto' }}>
+        <h2>{game.name}</h2>
+        <div className="card-action">
+          <button onClick={() => toggleFavorites(game)}>
+            {isInFavorites(game) ? '🩷' : '💔'}
+          </button>
+          <button onClick={() => togglePlayed(game)}>
+            {isInPlayed(game) ? '✅' : '❎'}
+          </button>
+        </div>
       </div>
     </div>
   )
